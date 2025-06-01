bffab5822f4f312790da4ed3e2cc1518a6efc54b
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2025-05-31 19:53:02 -0500

    make CgsModule pure virtual

--- src/GameShared/GameClasses/Module/CgsModule.h
+++ src/GameShared/GameClasses/Module/CgsModule.h
@@ -7,26 +7,27 @@ namespace CgsModule
     class Module
     {
     public:
-        Module();
-        Module(const Module& other);
+        //Module();
+        //Module(const Module& other);
+        virtual ~Module() = default;
 
-        virtual void Construct();
-        virtual bool Prepare();
-        virtual bool Release();
-        virtual void Destruct();
-        virtual void Update();
-        virtual void SetMultiThreaded(bool isMultiThreaded);
-        virtual void LockForInput();
-        virtual void UnlockForInput();
-        virtual void LockForOutput();
-        virtual void UnlockForOutput();
+        virtual void Construct() = 0;
+        virtual bool Prepare() = 0;
+        virtual bool Release() = 0;
+        virtual void Destruct() = 0;
+        virtual void Update() = 0;
+        virtual void SetMultiThreaded(bool isMultiThreaded) = 0;
+        virtual void LockForInput() = 0;
+        virtual void UnlockForInput() = 0;
+        virtual void LockForOutput() = 0;
+        virtual void UnlockForOutput() = 0;
 
     protected:
-        bool mbIsNewModule;
+        bool mbIsNewModule = false;
 
-        virtual bool DestroyInputDataStructure(DataStructure* pDataStructure);
-        virtual bool DestroyOutputDataStructure(DataStructure* pDataStructure);
-        virtual bool PrepareDataStructures(DataStructure* pInputDataStructure, DataStructure* pOutputDataStructure);
-        virtual bool ReleaseDataStructures(DataStructure* pInputDataStructure, DataStructure* pOutputDataStructure);
+        virtual bool DestroyInputDataStructure(DataStructure* pDataStructure) = 0;
+        virtual bool DestroyOutputDataStructure(DataStructure* pDataStructure) = 0;
+        virtual bool PrepareDataStructures(DataStructure* pInputDataStructure, DataStructure* pOutputDataStructure) = 0;
+        virtual bool ReleaseDataStructures(DataStructure* pInputDataStructure, DataStructure* pOutputDataStructure) = 0;
     };
 }
